f553f638181696f63f6853e8f879fd649cc6b03c
hayuse-0705 <h21149048@gmail.com>
2022-05-24 09:29:04 +0900

    load fade

--- www/dist/assets/js/script.js
+++ www/dist/assets/js/script.js
@@ -1,3 +1,5 @@
+$(window).bind("unload",function(){});
+
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -41,4 +43,34 @@ $(function () {
       );
     }
   });
+});
+
+$('.slick-arrow').click(function(){
+  window.location.href = '#';
+});
+
+$("a").on('click',function(event){
+  event.preventDefault();
+  var linkUrl = $(this).attr('href');
+  
+  // ここにリンク先への遷移直前に実行する内容を記述
+  $('.logo-load').addClass('fade-out');
+  $('.text-load').addClass('text-out');
+  $('.text-load').children('a').addClass('text-out');
+  $('.text-load-sub').addClass('text-out');
+  $('.text-load-sub').children('a').addClass('text-out');
+  $('.text-load-sub').addClass('box-out');
+  $('#humberger').removeClass('.hum_active');
+  $('#humberger').children('.hum_top').removeClass('hum_top_active');
+  $('#humberger').children('.hum_mid').removeClass('hum_mid_active');
+  $('#humberger').children('.hum_bot').removeClass('hum_bot_active');
+  $('.menu').removeClass("menu_open");
+  
+
+  function action() {
+    location.href = linkUrl;
+
+    // ここにリンク先への移動直後に実行する内容を記述
+  }
+  setTimeout(action,1000);
 });
